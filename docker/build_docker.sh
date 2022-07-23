fdfa3e1f663690b47b8c13a7939d799c15633428
RamiKrispin <ramkrisp@umich.edu>
2022-07-23 05:41:49 -0700

    add tag argument

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -2,11 +2,20 @@
 
 echo "Build the docker"
 
-docker build . -t rkrispin/ggplot2_workshop:dev.0.0.0.9000
+if [[ $1 = "" ]] ; then
+  echo "No tag argument, using default setting"
+  tag="rkrispin/ggplot2_workshop:dev.0.0.0.9000"
+  echo "Tag was set as $tag"
+else
+  tag=$1
+fi
+
+
+docker build . -t $tag
 
 if [[ $? = 0 ]] ; then
 echo "Pushing docker..."
-docker push rkrispin/ggplot2_workshop:dev.0.0.0.9000
+docker push $tag
 else
 echo "Docker build failed"
 fi
